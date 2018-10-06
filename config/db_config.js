6b773ae2c2f88dcc183fd5e8d697f470e40ced13
Juan Bedoya <juansb827@gmail.com>
2018-10-06 14:24:45 -0500

    fixed db config

--- config/db_config.js
+++ config/db_config.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
   "development": {
-    "url": "postgres://leutaewziszjhl:10f209c03974acdef69288a4f65655fe64f689f3215fd549d870dcacaf41bb5e@ec2-107-20-193-202.compute-1.amazonaws.com:5432/d3mq1oha600pi7",
+    "url": process.env.DB_CONNECTION_URL,
     "dialect": "postgres",
     "ssl": true  ,
     dialectOptions: {
